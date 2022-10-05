00a8c61fa0403c3a051cd4c2b5bf348ac3e9aa63
lianggan13 <676021630@qq.com>
2022-10-05 21:17:11 +0800

    s

---
+++ Concurrency/Set.cs
@@ -0,0 +1,265 @@
+﻿using Nito.AsyncEx;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Threading.Channels;
+using System.Threading.Tasks.Dataflow;
+using Utilities;
+
+
+namespace Concurrency
+{
+    public class Set
+    {
+        public static void Show()
+        {
+            //ConcurrentDictionary();
+
+            BufferBlock();
+        }
+
+        public static void ImmutableStack()
+        {
+            ImmutableStack<int> stack = ImmutableStack<int>.Empty;
+            stack = stack.Push(13);
+            ImmutableStack<int> biggerStack = stack.Push(7);
+
+            // Displays "7" followed by "13".
+            foreach (int item in biggerStack)
+                Trace.WriteLine(item);
+
+            // Only displays "13".
+            foreach (int item in stack)
+                Trace.WriteLine(item);
+        }
+
+        public static void ImmutableQueue()
+        {
+            ImmutableQueue<int> queue = ImmutableQueue<int>.Empty;
+            queue = queue.Enqueue(13);
+            queue = queue.Enqueue(7);
+
+            // Displays "13" followed by "7".
+            foreach (int item in queue)
+                Trace.WriteLine(item);
+
+            int nextItem;
+            queue = queue.Dequeue(out nextItem);
+            // Displays "13"
+            Trace.WriteLine(nextItem);
+        }
+
+        public static void ImmutableList()
+        {
+            ImmutableList<int> list = ImmutableList<int>.Empty;
+            list = list.Insert(0, 13);
+            list = list.Insert(0, 7);
+
+            // Displays "7" followed by "13".
+            foreach (int item in list)
+                Trace.WriteLine(item);
+
+            list = list.RemoveAt(1);
+        }
+
+        public static void ImmutableHashSet()
+        {
+            ImmutableHashSet<int> hashSet = ImmutableHashSet<int>.Empty;
+            hashSet = hashSet.Add(13);
+            hashSet = hashSet.Add(7);
+
+            // Displays "7" and "13" in an unpredictable order.
+            foreach (int item in hashSet)
+                Trace.WriteLine(item);
+
+            hashSet = hashSet.Remove(7);
+        }
+
+        public static void ImmutableDictionary()
+        {
+            ImmutableDictionary<int, string> dictionary = ImmutableDictionary<int, string>.Empty;
+            dictionary = dictionary.Add(10, "Ten");
+            dictionary = dictionary.Add(21, "Twenty-One");
+            dictionary = dictionary.SetItem(10, "Diez");
+
+            // Displays "10Diez" and "21Twenty-One" in an unpredictable order.
+            foreach (KeyValuePair<int, string> item in dictionary)
+                Trace.WriteLine(item.Key + item.Value);
+
+            string ten = dictionary[10];
+            // ten == "Diez"
+
+            dictionary = dictionary.Remove(21);
+        }
+
+        public static void ConcurrentDictionary()
+        {
+            var dictionary = new ConcurrentDictionary<int, string>();
+            string newValue = dictionary.AddOrUpdate(0,
+               addValueFactory: key =>
+               {
+                   string value = "Zero";
+                   return value;
+               },
+               updateValueFactory: (key, oldValue) =>
+               {
+                   string newValue = "Zero";
+                   return newValue;
+               });
+
+
+            // Using the same "dictionary" as above.
+            // Adds (or updates) key 0 to have the value "Zero".
+            dictionary[0] = "Zero1";
+
+
+            // Using the same "dictionary" as above.
+            bool keyExists = dictionary.TryGetValue(0, out string currentValue);
+
+
+            // Using the same "dictionary" as above.
+            bool keyExisted = dictionary.TryRemove(0, out string removedValue);
+        }
+
+
+        public static void BlockingCollection()
+        {
+            BlockingCollection<int> blockingQueue = new BlockingCollection<int>();
+
+            blockingQueue.Add(7);
+            blockingQueue.Add(13);
+            blockingQueue.CompleteAdding();
+
+            // Displays "7" followed by "13".
+            foreach (int item in blockingQueue.GetConsumingEnumerable())
+                Trace.WriteLine(item);
+        }
+
+        public static void BufferBlock()
+        {
+            var _asyncQueue = new BufferBlock<int>(
+                new DataflowBlockOptions { BoundedCapacity = 1 });
+            // Producer code
+            Task.Run(async () =>
+            {
+                // This Send completes immediately.
+                await _asyncQueue.SendAsync(7);
+
+                // This Send (asynchronously) waits for the 7 to be removed
+                // before it enqueues the 13.
+                await _asyncQueue.SendAsync(13);
+                _asyncQueue.Complete();
+            });
+
+            // Consumer code
+            Task.Run(async () =>
+            {
+                // Displays "7" followed by "13".
+                while (await _asyncQueue.OutputAvailableAsync()) // just for a single consumer
+                    LogHelper.Info($"{await _asyncQueue.ReceiveAsync()}");
+            });
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    int item;
+                    try
+                    {
+                        item = await _asyncQueue.ReceiveAsync(); // for multiple consumers
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        LogHelper.Error(ex.Message, ex);
+                        break;
+                    }
+                    LogHelper.Info($"{item}");
+                }
+            });
+        }
+
+        public static async Task AsyncProducerConsumerQueue()
+        {
+            var _asyncQueue = new AsyncProducerConsumerQueue<int>();
+
+            // Producer code
+            await _asyncQueue.EnqueueAsync(7);
+            await _asyncQueue.EnqueueAsync(13);
+            _asyncQueue.CompleteAdding();
+
+            // Consumer code
+            // Displays "7" followed by "13".
+            while (await _asyncQueue.OutputAvailableAsync())
+                Trace.WriteLine(await _asyncQueue.DequeueAsync());
+
+            while (true)
+            {
+                int item;
+                try
+                {
+                    item = await _asyncQueue.DequeueAsync();
+                }
+                catch (InvalidOperationException)
+                {
+                    break;
+                }
+                Trace.WriteLine(item);
+            }
+        }
+
+        public static async Task AsyncCollection()
+        {
+            var _asyncStack = new AsyncCollection<int>(
+                new ConcurrentStack<int>(), maxCount: 1);
+            var _asyncBag = new AsyncCollection<int>(
+                new ConcurrentBag<int>());
+
+            // Producer code
+            await _asyncStack.AddAsync(7);
+            await _asyncStack.AddAsync(13);
+            _asyncStack.CompleteAdding();
+
+            // Consumer code
+            // Displays "13" followed by "7".
+            while (await _asyncStack.OutputAvailableAsync())
+                Trace.WriteLine(await _asyncStack.TakeAsync());
+
+            while (true)
+            {
+                int item;
+                try
+                {
+                    item = await _asyncStack.TakeAsync();
+                }
+                catch (InvalidOperationException)
+                {
+                    break;
+                }
+                Trace.WriteLine(item);
+            }
+        }
+
+        public static void ChannelQueue()
+        {
+            Channel<int> queue = Channel.CreateBounded<int>(10);
+
+            // Producer code
+            ChannelWriter<int> writer = queue.Writer;
+            Task.Run(async () =>
+            {
+                await writer.WriteAsync(7);
+                await writer.WriteAsync(13);
+                writer.Complete();
+            });
+
+            // Consumer code
+            ChannelReader<int> reader = queue.Reader;
+            Task.Run(async () =>
+            {
+                while (await reader.WaitToReadAsync())
+                    while (reader.TryRead(out int value))
+                        Trace.WriteLine(value);
+            });
+        }
+    }
+}
---
+++ Concurrency/TAP.cs
@@ -0,0 +1,209 @@
+﻿using System.Net;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+using Utilities;
+
+namespace Concurrency
+{
+    public class TAP
+    {
+        public static async Task Show()
+        {
+
+
+            {
+                var ss = ObservableAsync(new HttpClient());
+                //LogHelper.Info($"{ss.Content.ReadAsStringAsync().Result}");
+
+                var sss = await ss.Publish();
+                LogHelper.Info($"{sss.Content.ReadAsStringAsync().Result}");
+
+                //ss.Subscribe(x => LogHelper.Info($"{x.Content.ReadAsStringAsync().Result}"));
+            }
+
+            return;
+
+            {
+                WebClient webClient = new WebClient();
+                Uri uri = new Uri("https://www.baidu.com/");
+                string content = await TAP.DownloadStringTaskAsync(webClient, uri);
+                LogHelper.Info(content);
+            }
+
+            {
+                WebRequest request = WebRequest.Create(new Uri("https://www.baidu.com/"));
+                WebResponse response = await TAP.GetResponseAsync(request);
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new System.IO.StreamReader(responseStream, Encoding.Default))
+                    {
+                        string content = reader.ReadToEnd();
+                        LogHelper.Info(content);
+                    }
+                }
+            }
+
+            {
+                try
+                {
+                    string result = await TAP.AuthorizeAsync(1);
+                    LogHelper.Info(result);
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Error(ex.Message, ex);
+                }
+            }
+
+            {
+                BlockAsObservable();
+
+                BlockAsObserver();
+            }
+        }
+
+        // EAP(+OperationAsync、+OperationCompleted) --> TAP(+TaskCompletionSource)
+        public static Task<string> DownloadStringTaskAsync(WebClient client,
+                Uri address)
+        {
+            var tcs = new TaskCompletionSource<string>();
+
+            // The event handler will complete the task and unregister itself.
+            DownloadStringCompletedEventHandler handler = null;
+            handler = (_, e) =>
+            {
+                client.DownloadStringCompleted -= handler;
+                if (e.Cancelled)
+                    tcs.TrySetCanceled();
+                else if (e.Error != null)
+                    tcs.TrySetException(e.Error);
+                else
+                    tcs.TrySetResult(e.Result);
+            };
+
+            // Register for the event and *then* start the operation.
+            client.DownloadStringCompleted += handler;
+            client.DownloadStringAsync(address);
+
+            return tcs.Task;
+        }
+
+        // AMP(+BeginOperation、+EndOperation) --> TAP(+FromAsync)
+        public static Task<WebResponse> GetResponseAsync(WebRequest client)
+        {
+            Task<WebResponse> task = Task<WebResponse>.Factory.FromAsync(client.BeginGetResponse,
+                client.EndGetResponse, null);
+            return task;
+        }
+
+        // Method callback --> TAP(+TaskCompletionSource)
+        public static Task<string> AuthorizeAsync(int workNo)
+        {
+            var tcs = new TaskCompletionSource<string>();
+            Authorize(workNo, (result, exception) =>
+            {
+                if (exception != null)
+                    tcs.TrySetException(exception);
+                else
+                    tcs.TrySetResult(result);
+            });
+
+            return tcs.Task;
+        }
+
+        public static void Authorize(int workNo, Action<string, Exception?> callback)
+        {
+            string msg;
+            Exception? ex = null;
+            if (workNo != 13)
+            {
+                msg = "Authorization failed";
+                ex = new Exception("WorkNo error");
+            }
+            else
+            {
+                msg = "Authorization Success";
+            }
+
+            callback?.Invoke(msg, ex);
+        }
+
+        public static async Task ParallelAsync()
+        {
+            var source = Enumerable.Range(0, 10);
+            Action<int> body = x => { Console.Write($"{x} "); };
+
+            await Task.Run(() => Parallel.ForEach(source, body));
+
+            Console.WriteLine();
+        }
+
+
+        public static async Task ObservableAsync()
+        {
+            IObservable<int> observable = Observable.Range(0, 10);
+            int lastElement = await observable.LastAsync();
+            int lastElement2 = await observable;
+            int nextElement = await observable.FirstAsync();
+            IList<int> allElements = await observable.ToList();
+            LogHelper.Info(string.Join(" ", allElements));
+        }
+
+        public static IObservable<HttpResponseMessage> ObservableAsync(HttpClient client)
+        {
+            IObservable<HttpResponseMessage> obs = null;
+
+            {
+                Task<HttpResponseMessage> task =
+                             client.GetAsync("http://www.example.com/");
+                obs = task.ToObservable();
+            }
+
+            {
+                obs = Observable.StartAsync(
+                            token => client.GetAsync("http://www.example.com/", token));
+            }
+
+            {
+                obs = Observable.FromAsync(
+                    token => client.GetAsync("http://www.example.com/", token));
+            }
+
+            {
+                IObservable<string> urls = new string[] { "http://www.example.com/" }.ToObservable();
+                obs = urls.SelectMany((url, token) => client.GetAsync(url, token));
+            }
+            return obs;
+        }
+
+        // Block(BufferBlock) --> Observable
+        public static void BlockAsObservable()
+        {
+            var buffer = new BufferBlock<int>();
+            IObservable<int> integers = buffer.AsObservable();
+            integers.Subscribe(
+                data => LogHelper.Info($"{data}"),
+                ex => LogHelper.Error(ex.Message, ex),
+                () => LogHelper.Info("Done"));
+
+            buffer.Post(13);
+            buffer.Complete();
+        }
+
+        // Block(ActionBlock) --> Observer
+        public static void BlockAsObserver()
+        {
+            IObservable<DateTimeOffset> ticks =
+                    Observable.Interval(TimeSpan.FromSeconds(1))
+                    .Timestamp()
+                    .Select(x => x.Timestamp)
+                    .Take(5);
+
+            var display = new ActionBlock<DateTimeOffset>(x => LogHelper.Info($"{x}"));
+            ticks.Subscribe(display.AsObserver()); // observer#1
+            ticks.Subscribe(t => LogHelper.Info($"{t.Second}")); // observer#2
+        }
+    }
+}
